d39a9abe7e0dcea48806d81a56b8cd72589ffd49
Alex Scarborough <alex@teak.io>
2024-03-05 16:00:40 -0800

    Minimize the amount of time we have our tilt renderer override in place.

--- lib/middleman-newsletter/newsletter.rb
+++ lib/middleman-newsletter/newsletter.rb
@@ -10,9 +10,6 @@ module Middleman
     end
 
     def render(opts = {}, locs = {}, &block)
-      # Ensure we render using our custom email renderer
-      ::Tilt.register(source_file.downcase, ::Middleman::Renderers::KramdownEmailTemplate)
-
       unless opts.key?(:layout)
         opts[:layout] = metadata[:options][:newsletter_layout]
         opts[:layout] = newsletter_options.layout if opts[:layout].nil? || opts[:layout] == :_auto_layout
@@ -21,8 +18,15 @@ module Middleman
         opts[:layout] = opts[:layout].to_s if opts[:layout].is_a? Symbol
       end
 
-      content = super(opts, locs, &block)
+      content = with_email_renderer { super(opts, locs, &block) }
+
       Premailer.new(content, with_html_string: true, output_encoding: 'UTF-8', input_encoding: 'UTF-8').to_inline_css
+    end
+
+    def with_email_renderer
+      # Ensure we render using our custom email renderer
+      ::Tilt.register(source_file.downcase, ::Middleman::Renderers::KramdownEmailTemplate)
+      yield
     ensure
       # Don't pollute the tilt mappings, will slow things down
       ::Tilt.default_mapping.unregister(source_file.downcase)
